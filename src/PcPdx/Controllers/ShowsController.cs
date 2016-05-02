fc1ffc1a979984e2a8aa5524737a41e9380a5487
lawtonB <lawton.a.browning@gmail.com>
2016-05-02 14:32:33 -0700

    tie shows and users together by applicationuserID

--- src/PcPdx/Controllers/ShowsController.cs
+++ src/PcPdx/Controllers/ShowsController.cs
@@ -11,16 +11,25 @@ using Microsoft.Data.Entity;
 using System.Diagnostics;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.AspNet.Authorization;
-
+using System.Security.Claims;
 
 namespace PcPdx.Controllers
 {
     public class ShowsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ShowsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View(db.Shows.ToList());
+            return View(_db.Shows.ToList());
         }
 
         public IActionResult Create()
@@ -29,39 +38,41 @@ namespace PcPdx.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Show show)
+        public async Task<IActionResult> Create(Show show)
         {
-            db.Shows.Add(show);
-            db.SaveChanges();
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            show.User = currentUser;
+            _db.Shows.Add(show);
+            _db.SaveChanges();
             return RedirectToAction("Index");
 
         }
         public IActionResult Edit(int id)
         {
-            var thisShow = db.Shows.FirstOrDefault(shows => shows.ShowId == id);
+            var thisShow = _db.Shows.FirstOrDefault(shows => shows.ShowId == id);
             return View(thisShow);
         }
 
         [HttpPost]
         public IActionResult Edit(Show show)
         {
-            db.Entry(show).State = EntityState.Modified;
-            db.SaveChanges();
+            _db.Entry(show).State = EntityState.Modified;
+            _db.SaveChanges();
             return RedirectToAction("index");
         }
 
         public IActionResult Delete(int id)
         {
-            var thisShow = db.Shows.FirstOrDefault(shows => shows.ShowId == id);
+            var thisShow = _db.Shows.FirstOrDefault(shows => shows.ShowId == id);
             return View(thisShow);
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            var thisShow = db.Shows.FirstOrDefault(shows => shows.ShowId == id);
-            db.Shows.Remove(thisShow);
-            db.SaveChanges();
+            var thisShow = _db.Shows.FirstOrDefault(shows => shows.ShowId == id);
+            _db.Shows.Remove(thisShow);
+            _db.SaveChanges();
             return RedirectToAction("index");
         }
     }
